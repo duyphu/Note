29f8a9f24910fdc791305d4caa3acad3fd63fe85
phund <phund@rikkeisoft.vn>
2016-02-24 08:58:09 +0700

    create project

---
+++ app/src/main/java/com/example/note/activity/NewNoteActivity.java
@@ -0,0 +1,14 @@
+package com.example.note.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by phund on 2/24/2016.
+ */
+public class NewNoteActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
